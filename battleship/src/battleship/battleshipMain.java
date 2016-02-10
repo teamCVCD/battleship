b55043c1f232c240ef7b2e60a99df4b6fbe64fb2
LimeDog <echindev@gmail.com>
2016-02-10 14:43:11 -0500

    Update 2/10/2016

--- battleship/src/battleship/battleshipMain.java
+++ battleship/src/battleship/battleshipMain.java
@@ -1,11 +1,12 @@
 package battleship;
 
 public class battleshipMain {
-	public static void String(System[]args){
+	public static void main(String[]args){
 		int arycoords[][]= new int[10][10];
 		for(int i=0;arycoords.length<10;i++){
+			System.out.println(i);
 			for(int j=0;arycoords.length<10;j++){
-				System.out.println(i+" "+j);
+				System.out.print(j);
 				
 			}
 		}
